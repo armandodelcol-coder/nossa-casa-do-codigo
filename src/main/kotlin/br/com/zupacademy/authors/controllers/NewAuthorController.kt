af320f5ec8f877b51263aba7a158ae5e90990fe0
Armando Tadeu <atdc.codemaster@gmail.com>
2021-06-29 18:43:39 -0300

    custom status

--- src/main/kotlin/br/com/zupacademy/authors/controllers/NewAuthorController.kt
+++ src/main/kotlin/br/com/zupacademy/authors/controllers/NewAuthorController.kt
@@ -2,10 +2,13 @@ package br.com.zupacademy.authors.controllers
 
 import br.com.zupacademy.authors.dtos.NewAuthorRequest
 import br.com.zupacademy.authors.repository.AuthorRepository
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
+import java.net.URI
 import javax.validation.Valid
 
 @Validated
@@ -13,10 +16,12 @@ import javax.validation.Valid
 class NewAuthorController(val authorRepository: AuthorRepository) {
 
     @Post
-    fun register(@Body @Valid request: NewAuthorRequest) {
+    fun register(@Body @Valid request: NewAuthorRequest): HttpResponse<Any> {
         println(request)
         val author = request.toAuthor()
         authorRepository.save(author)
+        val uri: URI = UriBuilder.of("/autores/{id}").expand(mutableMapOf(Pair("id", author.id)))
+        return HttpResponse.created(uri)
     }
 
 }
