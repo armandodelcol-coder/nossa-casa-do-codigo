0609bba77f4e98145c4f848bd0d5af6bf4f00344
Armando Tadeu <atdc.codemaster@gmail.com>
2021-06-29 11:36:39 -0300

    initial validation of author request

--- src/main/kotlin/br/com/zupacademy/authors/controllers/NewAuthorController.kt
+++ src/main/kotlin/br/com/zupacademy/authors/controllers/NewAuthorController.kt
@@ -4,12 +4,15 @@ import br.com.zupacademy.authors.dtos.NewAuthorRequest
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autores")
 class NewAuthorController {
 
     @Post
-    fun register(@Body request: NewAuthorRequest): String {
+    fun register(@Body @Valid request: NewAuthorRequest): String {
         return request.toString()
     }
 
--- src/main/kotlin/br/com/zupacademy/authors/dtos/NewAuthorRequest.kt
+++ src/main/kotlin/br/com/zupacademy/authors/dtos/NewAuthorRequest.kt
@@ -1,7 +1,13 @@
 package br.com.zupacademy.authors.dtos
 
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@Introspected
 data class NewAuthorRequest(
-    val name: String,
-    val email: String,
-    val description: String
+    @field:NotBlank val name: String,
+    @field:NotBlank @field:Email val email: String,
+    @field:NotBlank @field:Size(max = 400) val description: String
 )
