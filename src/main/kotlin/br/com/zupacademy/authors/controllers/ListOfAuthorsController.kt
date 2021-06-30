08835a8f0d288324645e14316baa95cfa4be94dd
Armando Tadeu <atdc.codemaster@gmail.com>
2021-06-30 10:31:26 -0300

    filter by email

--- src/main/kotlin/br/com/zupacademy/authors/controllers/ListOfAuthorsController.kt
+++ src/main/kotlin/br/com/zupacademy/authors/controllers/ListOfAuthorsController.kt
@@ -2,16 +2,28 @@ package br.com.zupacademy.authors.controllers
 
 import br.com.zupacademy.authors.dtos.AuthorDetailsResponse
 import br.com.zupacademy.authors.repository.AuthorRepository
+import io.micronaut.data.model.Page
+import io.micronaut.data.model.Pageable
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
 
 @Controller("/autores")
 class ListOfAuthorsController(private val authorRepository: AuthorRepository) {
 
     @Get
-    fun listAll(): HttpResponse<List<AuthorDetailsResponse>> {
-        val authors = authorRepository.findAll()
+    fun listAll(
+        @QueryValue(defaultValue = "") email: String,
+        pageable: Pageable
+    ): MutableHttpResponse<Page<AuthorDetailsResponse>>? {
+        if (email.isBlank()) {
+            val authors = authorRepository.findAll(pageable)
+            return HttpResponse.ok(authors.map { author -> AuthorDetailsResponse(author) })
+        }
+
+        val authors = authorRepository.findByEmailContaining(email, pageable)
         return HttpResponse.ok(authors.map { author -> AuthorDetailsResponse(author) })
     }
 
--- src/main/kotlin/br/com/zupacademy/authors/repository/AuthorRepository.kt
+++ src/main/kotlin/br/com/zupacademy/authors/repository/AuthorRepository.kt
@@ -3,10 +3,14 @@ package br.com.zupacademy.authors.repository
 import br.com.zupacademy.authors.entities.Author
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
+import io.micronaut.data.model.Page
+import io.micronaut.data.model.Pageable
 import java.util.*
 
 @Repository
 interface AuthorRepository : JpaRepository<Author, Long> {
+
+    fun findByEmailContaining(email: String, pageable: Pageable): Page<Author>
 
     fun findByCode(code: String): Optional<Author>
 
